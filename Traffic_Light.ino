b96e2c87f98ea081b9ab373c1341366e9c77b993
Sriram <sriram1618033@gmail.com>
2023-07-08 22:59:26 +0530

    Initial commit

---
+++ Traffic_Light.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(0,0);
+digitalWrite(1,1);
+delay(2000);
+digitalWrite(0,1);
+digitalWrite(1,0);
+delay(2000);
+}
